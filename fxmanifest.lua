dc0431908f3a8ef5312b415be135ada4fe3019ba
Zee <52096074+Zelo80@users.noreply.github.com>
2023-01-12 14:56:26 -0600

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,19 @@
-game 'rdr3'
-fx_version 'adamant'
+fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+games {"rdr3"}
 
-ui_page "html/index.html"
-
-client_script 'client.lua'
-
-server_scripts {
-    '@mysql-async/lib/MySQL.lua',
-    'server.lua'
+client_scripts {
+    "client/client.lua",
 }
 
-files {
-    "html/index.html",
-    "html/styles.css",
-    "html/eden.png",
-    "html/telegram.png",
-    "html/telegram_background.png",
-    "html/telegram_divider.png",
-    "html/telegram_footer.png",
-    "html/telegram_header.png",
-    "html/reset.css",
-    "html/listener.js"
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    "server/server.lua"
+}
+
+shared_scripts {
+    "config.lua",
+    "hairs.lua",
+    'overlays.lua',
+    'client/npc.lua',
 }
